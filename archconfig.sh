6da7031223998a904feca91733d7bd97cdf0b852
[qwerty] <60716753+qwerty-1871@users.noreply.github.com>
2023-05-04 19:52:28 -0500

    Add files via upload

--- archconfig.sh
+++ archconfig.sh
@@ -5,17 +5,22 @@ uidd='notdone'
 kerneld='notdone'
 doned='donenot'
 graphicsd='notdone'
+desktopd='notdone'
+yayd='notdone'
+vboxd='notdone'
+xwd='notdone'
+blued='notdone'
 sdr=$PWD
 shopt -s lastpipe
-#check if script is being run as root; abort if not
+#check if script is being run as root; abort if it is
 if [ "$EUID" = 0 ]; then 
   echo "This script should not be run as root"
   exit
 fi
 #start of script, asks for user confirmation before beginning process
-echo Welcome to Archconfig!
-echo If you have not read the .readme file on Github yet, please do so before continuing
-echo "Enter 'start' to begin the installation process or 'stop' to abort"
+echo "Welcome to Archconfig!
+If you have not read the .readme file on Github yet, please do so before continuing
+Enter 'start' to begin the installation process or 'stop' to abort"
 while [ $begind != 'done' ]; do
     read begin
     case $begin in 
@@ -23,58 +28,138 @@ while [ $begind != 'done' ]; do
         begind='done' ;;
     stop) 
         exit ;;
+    qwerty)
+        ;;
     *) echo "Invalid input" ;;
     esac
 done
-#asks for username and then checks /home for answer validity
-#echo What is your username?
-#while [ $uidd != 'done' ]; do
-#    read uid
-#    if [ -d /home/$uid ]; then
-#        uidd="done"
-#    else
-#        echo Not a user
-#    fi
-#done
 #asks what type of kernel is being used
-echo "Are you using a version of the kernel other than the default? (y/n)"
-echo Note that this includes linux-lts
-echo If you do not know the answer to this, it is probably no.
+echo "Are you using a version of the kernel other than the default? (y/n)
+Note that this includes linux-lts
+If you do not know the answer to this, it is probably no."
 while [ $kerneld != 'done' ]; do
     read kernel
     case $kernel in
-    y) 
-    kerneld='done' ;;
-    n)
+    y | n) 
     kerneld='done' ;;
     *)
     echo Invalid input ;;
     esac
 done
-echo "What type of graphics are you using?"
-echo "1) NVIDIA"
-echo "2) AMD"
-echo "3) Intel"
+#asks what type of graphics card is being used
+echo "What type of graphics are you using?
+1) NVIDIA
+2) AMD
+3) Intel"
 while [ $graphics != 'done' ]; do
     read graphics
     case $graphics in
-    1)
-    graphicsd='done' ;;
-    2)
-    graphicsd='done' ;;
-    3) 
+    1 | 2 | 3)
     graphicsd='done' ;;
     *)
     echo Invalid input ;;
     esac
 done
+#asks what desktop the user would like to install
+echo "What desktop environment would you like to use?
+1) GNOME
+2) KDE Plasma
+3) Cinnamon
+4) Xfce
+5) None of the above"
+while [ $desktopd != 'done' ]; do
+    read desktop
+    case $desktop in
+    1 | 2 | 3 | 4 | 5)
+    desktopd='done' ;;
+    *)
+    echo Invalid input ;;
+    esac
+done
+if [ $desktop = '5' ]; then
+    echo "Would you like to install a display server?
+    1) Yes, X11
+    2) Yes, Wayland
+    3) No"
+    while [ $xwd != 'done' ]; do
+        read xw
+        case $xw in
+        1 | 2 | 3)
+        xwd='done' ;;
+        *)
+        echo Invalid input ;;
+        esac
+    done
+fi
+#questions pertaining to individual packages
+echo "You will now be asked a series of questions asking if you would like a specific package that the script has special configurations or support for
+Each will expect a simple (y/n) answer. You may also enter h for a brief explanation of the package"
+read -p "Press ENTER to continue"
+echo Would you like to install support for Bluetooth?
+while [ $blued != 'done' ]; do
+    read blue
+    case $blue in
+    y | n)
+    yayd='done' ;;
+    h)
+    echo Bluetooth is a short-range networking standard. Saying yes will install the bluez daemon and the blueman GUI ;;
+    *)
+    echo Invalid input ;;
+    esac
+done
+echo Would you like to install yay?
+while [ $yayd != 'done' ]; do
+    read yay
+    case $yay in
+    y | n)
+    yayd='done' ;;
+    h)
+    echo Yay is a pacman wrapper and an AUR helper. Its primary purpose is to allow the user to conveniently and easily install and upgrade packages from the AUR ;;
+    *)
+    echo Invalid input ;;
+    esac
+done
+echo Would you like to install Virtualbox?
+while [ $vboxd != 'done' ]; do
+    read vbox
+    case $vbox in
+    y | n)
+    vboxd='done' ;;
+    h)
+    echo Virtualbox is a virtualizer which allows the user to emulate one or more virtual computers within their machine. ;;
+    *)
+    echo Invalid input ;;
+    esac
+done
+if [ $yay = 'y' ]; then
+    echo Would you like to install Spotify?
+    while [ $spotifyd != 'done' ]; do
+        read spotify
+        case $spotify in
+        y | n)
+        spotifyd='done' ;;
+        h)
+        echo Spotify is a properitary music streaming service ;;
+        *)
+        echo Invalid input ;;
+        esac
+    done
+fi
+echo "Are there any other packages you would like to install?
+If so, enter their exact names now. Do not use any seperation besides a single space
+Precision is important as mistyping the name of a package may break the command
+Simply enter nothing here if you do not wish to install anything else at this time"
+read packages
 #creates file to prevent sudo from being asked again
-echo "Enter your password to begin installation
-From now until the end of the script you will not have to interact with your computer"
+echo "Please enter your password now.
+This will prevent you from needing to do it again while the script runs."
 sudo touch /etc/sudoers.d/passwd_timeout
 sudo bash -c "echo 'Defaults timestamp_timeout=-1' >> /etc/sudoers.d/timestamp_timeout"
-#begins installing packages
-#install graphics drivers
+if [ $begin = qwerty ]; do
+    source $sdr/backend/qwerty.sh
+fi
+sudo mv -f $sdr/backend/dotfiles/pacman.conf /etc/pacman.conf
+pacman -Sy --noconfirm networkmanager devtools dkms go nano
 if [ $graphics = 1 ]; then
     pacman -Sy --noconfirm nvidia nvidia-utils
     elif [ $graphics = 2 ]; then
@@ -82,50 +167,53 @@ if [ $graphics = 1 ]; then
     else
     pacman -Sy --noconfirm xf86-video-intel mesa
 fi
-#install vbox drivers
-if [ $kernel = n ]; then
-    sudo pacman -Sy --noconfirm virtualbox-host-modules-arch
-    else
-    sudo pacman -Sy --noconfirm virtualbox-host-dkms
+if [[ $desktop = '2' || $desktop = '3' || $desktop = '4' || $xw = '1' ]]; then
+    sudo pacman -Sy --noconfirm xorg
+elif [[ $desktop = '1' || $xw = '2' ]]; then
+    sudo pacman -Sy --noconfirm wayland
 fi
-#pacman, first replacing pacman.conf
-sudo mv -f $sdr/backend/dotfiles/pacman.conf /etc/pacman.conf
-sudo pacman -Sy --noconfirm xorg
-sudo pacman -Sy --noconfirm go nano devtools dkms cinnamon lightdm lightdm-slick-greeter os-prober xed brasero rhythmbox libgpod gst-libav kpat xreader gnome-terminal kmahjongg gnome-mines neofetch steam openssh bluez bluez-utils firefox-developer-edition virtualbox gnome-screenshot libreoffice-fresh gimp lutris libnotify speech-dispatcher hunspell-en_US networkmanager hexchat discord
-#installing yay
-cd ~
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg
-sudo pacman -U --noconfirm /home/erika/yay/yay-*.pkg.tar.zst
-cd ~
-rm -rf yay
-#using yay to install other AUR packages
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | gpg --import -
-echo -ne '\n' | yay -S --answerclean None --answerdiff None --answeredit None --answerupgrade None pamac-nosnap xplayer-plparser-git xplayer-git mintstick pioneers spotify
-#moving dotfiles
-sudo mv -f $sdr/backend/dotfiles/.bashrc ~
-sudo mv -f $sdr/backend/dotfiles/.nanorc ~
-sudo mv -f $sdr/backend/dotfiles/grub /etc/default/grub
-sudo mv -f $sdr/backend/dotfiles/lightdm.conf /etc/lightdm/lightdm.conf
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-#enabling services
-sudo systemctl enable lightdm
-sudo systemctl enable bluetooth
-#end of the script
-echo The script has finished configuring your system
-echo Thank you for using Archconfig. Please share any issues you had with the script on the Github. Feedback can be emailed to qwerty1871@gmail.com
-echo Enter 'reboot' if you would like the script to reboot your system or 'exit' if you would like the script to exit without reboot
-while [ $doned != 'done' ]; do
-    read done
-    case $done in
-    reboot)
-    sudo rm /etc/sudoers.d/timestamp_timeout
-    sudo reboot now ;;
-    exit)
-    sudo rm /etc/sudoers.d/timestamp_timeout
-    exit ;;
-    *)
-    echo Invalid input
-    esac
-done
+if [[ $desktop = '2' || $desktop = '3' || $desktop = '4' ]]; then
+    sudo pacman -Sy --noconfirm lightdm lightdm-slick-greeter
+    sudo systemctl enable lightdm
+    sudo mv -f $sdr/backend/dotfiles/lightdm.conf /etc/lightdm/lightdm.con
+fi
+if [[ $desktop = '1' ]]; then
+    sudo pacman -Sy gnome
+    sudo systemctl enable gdm
+elif [[ $desktop = '2' ]]; then
+    sudo pacman -Sy plasma
+elif [[ $desktop = '3' ]]; then
+    sudo pacman -Sy cinnamon gnome-panel metacity gnome-screenshot
+elif [[ $desktop = '4' ]]; then
+    sudo pacman -Sy xfce4
+    sudo pacman -Sy xfce4-goodies
+fi
+if [[ $blue = 'y' ]]; then
+    sudo pacman -Sy bluez bluez-utils blueman
+    sudo systemctl enable bluetooth
+fi
+if [[ $yay = 'y' ]]; then
+    cd ~
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg
+    sudo pacman -U --noconfirm /home/erika/yay/yay-*.pkg.tar.zst
+    cd ~
+    rm -rf yay
+fi
+if [[ $vbox = 'y' ]]; then
+    if [ $kernel = n ]; then
+        sudo pacman -Sy --noconfirm virtualbox-host-modules-arch
+    else
+        sudo pacman -Sy --noconfirm virtualbox-host-dkms
+    fi
+    pacman -Sy --noconfirm virtualbox virtualbox-guest-iso 
+    if [[ $yay = 'y' ]]; then
+        echo -ne '\n' | yay -S --answerclean None --answerdiff None --answeredit None --answerupgrade None virtualbox-ext-oracle
+    fi
+fi
+if [[ $spotify = 'y' ]]; then 
+    echo -ne '\n' | yay -S --answerclean None --answerdiff None --answeredit None --answerupgrade None spotify
+fi
+source $sdr/backend/end.sh
+exit
